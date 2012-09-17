a73527183b70787f3a6b4ffeb7c48506cb60027d
Jefferson Almeida <jra@cin.ufpe.br>
2012-09-17 15:21:42 -0300

    javadoc adicionado para o metodo avalie

--- src/br/edu/ufcg/dsc/evaluation/Avaliador.java
+++ src/br/edu/ufcg/dsc/evaluation/Avaliador.java
@@ -87,16 +87,30 @@ public class Avaliador {
 	}
 	
 	/**
-	 * @param nome descrição para a descrição de argumentos.
-	   @return descrição para a descrição do valor de retorno.
-	   @exception nomeClasseExceção descrição para a descrição de exceções lançadas pelo método.
-	 * 
+	 * @author Jefferson Almeida - jra at cin dot ufpe dot br
+	 *  
+	 * @param Lines line: Escolha de qual linha de produtos será avaliada.  Mobile Media, Default ou Target
+	 * @param String sourcePath: O path do source da linha de produtos.  SPL Original  (SPL)
+	 * @param String targetPath: O path do source da linha de produto com a evolução/refactoring. (SPL')
+	 * @param int timeOut:   ????????
+	 * @param int qtdTestes: A quantidade de testes que será gerada para cada método da Linha de Produtos.
+	 * @param approach: Qual a abordagem que será utilizada. @see Approach.java
+	 * @param boolean temAspectosSource: Valor booleano para indicar se tem aspectos no source da LPS ou não.
+	 * @param boolean temAspectosTarget: Valor booleano para indicar se tem aspectos no source Target da LPS ou não.
+	 * @param String controladoresFachadas:
+	 * @param Criteria criteria: @see Criteria.java
+	 * @param CKFormat sourceCKKind: Informa o formato do CK presente no source da LPS Original: Hephaestus ou Formato Simples.  @see CKFormat.java
+	 * @param CKFormat targetCKKind: Informa o formato do CK presente no source da LPS Target: Hephaestus ou Formato Simples. @see CKFormat.java
+	 * @param AMFormat sourceAMFormat: Informa o formato do AM presente no source da LPS Original: Hephaestus ou Formato Simples @see AMFormat.java
+	 * @param AMFormat targetAMFormat: Informa o formato do AM presente no source da LPS Target: Hephaestus ou Formato Simples @see AMFormat.java
+	 * @param String... libs: Este parâmetro pode ser uma string ou várias. Uma sequência de bibliotecas.
+	 * @return void: Este método não retorna nada.
+	 * @throws DirectoryException: Lança uma exceção de diretório dos paths das linhas de produtos.
+	 * @exception AssetNotFoundException:  Lança uma exceção de asset não encontrado.
+	 * @exception IOException: Lança uma exceção de I/O.
+	 * @exception Err: Lança uma exceção de Erro.
 	 * */	
-	public void avalie(Lines line, String sourcePath, String targetPath, 
-			int timeout, int qtdTestes, Approach approach, 
-			boolean temAspectosSource, boolean temAspectosTarget,
-			String controladoresFachadas, Criteria criteria, CKFormat sourceCKKind, CKFormat targetCKKind,
-			AMFormat sourceAMFormat, AMFormat targetAMFormat, String... libs) throws DirectoryException{
+	public void avalie(Lines line, String sourcePath, String targetPath, int timeout, int qtdTestes, Approach approach, boolean temAspectosSource, boolean temAspectosTarget, String controladoresFachadas, Criteria criteria, CKFormat sourceCKKind, CKFormat targetCKKind, AMFormat sourceAMFormat, AMFormat targetAMFormat, String... libs) throws DirectoryException{
 		
 		ToolCommandLine toolCommandLine = new ToolCommandLine(line);
 		
@@ -119,35 +133,7 @@ public class Avaliador {
 					sourceCKKind, targetCKKind, sourceAMFormat, targetAMFormat, libPathSource, libPathTarget);
 			
 			boolean resultadoEstahCerto = true;
-			
-//			if(!resultado.isAssetMappingsEqual()){
-//				if(resultado.isRefinement()){ // Se ferramenta respondeu que eh refinamento, rodar 10x a quantidade de testes.
-//					resultado = this.verifyLine(toolCommandLine, sourcePath, targetPath, timeout, 10 * qtdTestes, 
-//							approach, temAspectosSource, temAspectosTarget, controladoresFachadas, method, 
-//							sourceCKKind, targetCKKind, sourceAMFormat, targetAMFormat);
-//					
-//					if(resultado.isRefinement()){
-//						System.out.println("O resultado provavelmente estah OK. Verificar manualmente.");
-//					}
-//					else{
-//						System.out.println("Apï¿½s rodar 10x a quantidade de testes, foram encontradas diferenï¿½as.\n" +
-//								"O resultado estï¿½ errado.");
-//					}
-//				}
-//				else{
-//					if(resultado.temMesmosMetodosPublicos()){
-//						System.out.println("A ferramenta identificou mudanï¿½a de comportamento em algum dos mï¿½todos,\n" +
-//								"ï¿½ necessï¿½rio checar se os mï¿½todos com comportamento modificado tem clientes.\n" +
-//								"Se existirem clientes e o comportamento deles tambï¿½m tiver sido modificado,\n" +
-//								"a resposta estï¿½ OK. Senï¿½o, a resposta estï¿½ errada.");
-//					}
-//					else{
-//						System.out.println("Projetos nï¿½o tem mesmos mï¿½todos pï¿½blicos\n" +
-//						"ï¿½ necessï¿½rio a verificaï¿½ï¿½o manual.");
-//					}
-//				}
-//			}
-			
+					
 			System.out.println(resultadoEstahCerto);
 			
 			String resultFileName = Constants.PLUGIN_PATH + Constants.FILE_SEPARATOR + 
